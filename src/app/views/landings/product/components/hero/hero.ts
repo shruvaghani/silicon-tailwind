8b41889de7751c109d5e6bfce8aaeec40ba31df0
shruvaghani <shrutivaghani2004@gmail.com>
2026-05-09 11:59:24 +0530

    fix: product page hero swiper

--- src/app/views/landings/product/components/hero/hero.ts
+++ src/app/views/landings/product/components/hero/hero.ts
@@ -1,7 +1,17 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  Inject,
+  PLATFORM_ID,
+  ViewChild,
+} from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import { register } from 'swiper/element/bundle';
 import { sliderImages } from '../../data';
+
 register();
 
 @Component({
@@ -11,6 +21,60 @@ register();
   styles: ``,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class Hero {
+  
+export class Hero implements AfterViewInit {
   sliderImages = sliderImages;
+
+  isBrowser: boolean;
+
+  @ViewChild('heroSwiper')
+  heroSwiper!: ElementRef;
+
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  settings = {
+    counter: false,
+    selector: 'a',
+  };
+
+  ngAfterViewInit(): void {
+    if (this.isBrowser) {
+      const swiperEl = this.heroSwiper.nativeElement;
+
+      Object.assign(swiperEl, {
+        slidesPerView: 1,
+        effect: 'coverflow',
+        centeredSlides: true,
+        loop: true,
+        speed: 650,
+
+        coverflowEffect: {
+          rotate: 0,
+          stretch: 0,
+          depth: 560,
+          modifier: 1,
+          slideShadows: true,
+        },
+
+        autoplay: {
+          delay: 6000,
+          disableOnInteraction: false,
+        },
+
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction',
+        },
+
+        navigation: {
+          prevEl: '.btn-prev',
+          nextEl: '.btn-next',
+        },
+      });
+
+      swiperEl.initialize();
+    }
+  }
 }
