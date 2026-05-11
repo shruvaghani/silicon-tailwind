e930b7e659d3f5f30f036d2586452881ad2f1683
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-05-11 17:28:49 +0530

    wip : saas-v5 features

--- src/app/views/landings/saas-v5/components/features/features.ts
+++ src/app/views/landings/saas-v5/components/features/features.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { ParallaxDirective } from '@/app/directive/parallax.directive';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'saas-v5-features',
   imports: [RouterLink, ParallaxDirective],
   templateUrl: './features.html',
   styles: ``,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-  
 export class Features {
 
 }
