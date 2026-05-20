3dcaf3d81ded1b506341d763555a1f23b27f8e25
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-05-20 11:42:46 +0530

    fix : swiper

--- src/app/views/landings/software-dev-agency-v1/components/hero/hero.ts
+++ src/app/views/landings/software-dev-agency-v1/components/hero/hero.ts
@@ -1,10 +1,13 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { heroSlidesData, socialLinksData } from '../../data';
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+import { SwiperOptions } from 'swiper/types';
+import { SwiperDirective } from '@/app/directive/swiper-directive';
 
 @Component({
   selector: 'software-dev-agency1-hero',
-  imports: [RouterLink],
+  imports: [RouterLink, SwiperDirective],
   templateUrl: './hero.html',
   styles: ``,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -12,4 +15,22 @@ import { heroSlidesData, socialLinksData } from '../../data';
 export class Hero {
   heroSlidesData = heroSlidesData;
   socialLinksData = socialLinksData;
+  swiperConfig: SwiperOptions = {
+    modules: [Navigation, EffectFade, Pagination],
+    effect: 'fade',
+    fadeEffect: { crossFade: true },
+    speed: 700,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '#hero-pagination',
+      clickable: true,
+    },
+    navigation: {
+      prevEl: '#hero-prev',
+      nextEl: '#hero-next',
+    },
+  };
 }
