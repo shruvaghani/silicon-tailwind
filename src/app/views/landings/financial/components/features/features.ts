b97c7b520cdef943bcc004b51b7838a2cdfe2397
PriyanshiLakhani <priyanshi.lakhani.it@gmail.com>
2026-05-20 12:25:13 +0530

    fix : angular json and swiper

--- src/app/views/landings/financial/components/features/features.ts
+++ src/app/views/landings/financial/components/features/features.ts
@@ -1,17 +1,40 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
 import { register } from 'swiper/element/bundle';
 import { featuresData } from '../../data';
+import { Pagination } from 'swiper/modules';
+import { SwiperOptions } from 'swiper/types';
+import { SwiperDirective } from '@/app/directive/swiper-directive';
 
 register();
 
 @Component({
   selector: 'financial-features',
-  imports: [RouterLink],
+  imports: [RouterLink,SwiperDirective],
   templateUrl: './features.html',
   styles: ``,
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Features {
   featuresData = featuresData;
+  swiperConfig: SwiperOptions = {
+    modules: [Pagination],
+    slidesPerView: 1,
+    spaceBetween: 16,
+    pagination: {
+      el: '#featurepagination',
+      clickable: true,
+    },
+    breakpoints: {
+      '500': {
+        slidesPerView: 2,
+      },
+      '800': {
+        slidesPerView: 3,
+      },
+      '1200': {
+        slidesPerView: 4,
+      },
+    },
+  };
 }
